5c298260ded34df38d4768596c48ee90e3f8f354
John Curry <fastpenguin91@gmail.com>
2019-12-14 15:32:37 -0800

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 //const db = require('./queries')
 const paginate = require('express-paginate');
 
